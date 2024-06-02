eb3fac8fa862ea01ba6cf704167ecaa319384ae3
Irfan Jadoon <115137229+jadoon04@users.noreply.github.com>
2024-06-02 20:04:43 +0500

    Add files via upload

---
+++ bridgepatterndemo/Assemble.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bridgepatterndemo;
+ class Assemble implements Workshop {
+    @Override
+    public void work()
+    {
+        System.out.print(" And");
+        System.out.println(" Assembled.");
+    }
+} 
